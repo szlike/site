778b65d5243e35df5af6ff6c6b4c7d65c839af13
changlmasaki <59718659+changlmasaki@users.noreply.github.com>
2022-01-17 15:33:26 -0500

    jira2 problems
    i have a issue mentions error "Uncaught Error: Team(...): Nothing was returned from render. This usually means a return statement is missing. Or, to render nothing, return null."
    Uncaught Error: Team(...): Nothing was returned from render. This usually means a return statement is missing. Or, to render nothing, return null.

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -1,78 +1,64 @@
-import React from "react";
-import { Row, Col } from "react-flexbox-grid";
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import ReactPaginate from 'react-paginate';
 import "./team.scss";
-// Components
-import TeamBox from './teamBox';
-import TeamInfo from "./teamInfo";
-import Title from "../ui-components/title/title";
-// Assets
-import Person01 from "../../assets/about/ceo.jpg"
-import Person02 from "../../assets/about/p1.jpg"
-import Person03 from '../../assets/about/p2.jpg'
-import Person04 from '../../assets/about/p3.png'
-import Person05 from '../../assets/about/p4.png'
 import $ from 'jquery'; 
-
-
-
-
-
-
-// arrow function
-const abo = function(){  
-}
-
-// closure
-// find the differences between using function keyword and using arrow function
-const abou = ()=>{
-  return true
-}
-
-const abouu = ()=> true
-
-// xmlhttpRequest
-// jquery
-// js fetch
-// axios
-
 
 const Team = () => {
   // property deconstruction
-  const [cannabisList, setCannabisList] = useState(0);
+  //const [cannabisList, setCannabisList] = useState(0);
+    const [postsPerPage] = useState(5);
+    const [offset, setOffset] = useState(1);
+    const [posts, setPosts] = useState([]);
+    const [pageCount, setPageCount] = useState(0)
 
-
-  const fireGetCannabis = function(){
-    $.ajax({
-      method : 'GET',
-      url : 'https://random-data-api.com/api/cannabis/random_cannabis?size=10',
-      dataType : 'json'
-    })
-    .done(enter)
-    .fail(function(e){
-      console.log('failed: ', e)
-    }) 
-  };
-
-  const enter = (data)=>{
-    $("#id").text(data.id)
-    console.log(data)
-
-    setCannabisList(data)
-  }
-
-  return (
-    <div id="about">
-    <div name='random-cannabis'>
-    <ul> https://random-data-api.com/api/cannabis/random_cannabis?size=10</ul>
-    <div>Output: <span id="#id">1234</span></div>
-    <button onClick={()=>fireGetCannabis()}>get random cannabis</button>
-    <input type='text'/> //size=100
-    {use react template loop to display {cannabisList} ul-> li}
-    <button>get specific number cannabis</button>
-    </div>
-    </div>
-  )
-  
+    const getData = (data) => {
+      return (
+        data.map(post => <div className="id" key={post.id}>
+          <p>Item ID: {post.id}</p>
+          <p>Item UID {post.uid}</p>
+        </div>)
+      )
+    
+    }
+    const getPosts = async () => {
+      const res = await axios.get(`https://random-data-api.com/api/cannabis/random_cannabis?size=10`)
+      const data = res.data;
+      const slice = data.slice(offset - 1 , offset - 1 + postsPerPage)
+      const postData = getData(slice)
+      setPosts(postData)
+      setPageCount(Math.ceil(data.length / postsPerPage))
+    }
+    
 }
+  
+   
+  //const fireGetCannabis = function(){
+    //axios.get('https://random-data-api.com/api/cannabis/random_cannabis?size=10')
+    //.then((response) => {
+    //  console.log(response.data.id);
+    //  console.log(response.data.uid);
+    
+  //const enter = (data)=>{
+    //$("#id").text(data.id)
+   // console.log(data)
+
+   // setCannabisList(data)
+  //}
+
+  //<input type='text'/> //size=100
+  //{use react template loop to display {cannabisList} ul-> li}
+ // return (
+   // <div id="about">
+   // <div name='random-cannabis'>
+   // <ul> https://random-data-api.com/api/cannabis/random_cannabis?size=10</ul>
+   // <div>Output: <span id="#id">1234</span></div>
+   // <button onClick={()=>fireGetCannabis()}>get random cannabis</button>
+
+   // <button>get specific number cannabis</button>
+   // </div>
+   // </div>
+  //)
+    
 
 export default Team;
