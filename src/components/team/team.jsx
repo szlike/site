f35d199e232103dac9eb2edbd6e4e0005c893545
changlmasaki <59718659+changlmasaki@users.noreply.github.com>
2022-01-19 19:04:53 -0500

    JIRA 3

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -5,35 +5,87 @@ import "./team.scss";
 import $ from 'jquery'; 
 
 const Team = () => {
-  // property deconstruction
-  //const [cannabisList, setCannabisList] = useState(0);
+  
     const [postsPerPage] = useState(5);
     const [offset, setOffset] = useState(1);
     const [posts, setPosts] = useState([]);
     const [pageCount, setPageCount] = useState(0)
-
-    const getData = (data) => {
-      return (
-        data.map(post => <div className="id" key={post.id}>
-          <p>Item ID: {post.id}</p>
-          <p>Item UID {post.uid}</p>
-        </div>)
-      )
     
-    }
-    const getPosts = async () => {
-      const res = await axios.get(`https://random-data-api.com/api/cannabis/random_cannabis?size=10`)
+    const getSpec = async() =>{
+      var input=document.getElementById('userInput').value;
+      document.getElementById("link").href = "https://random-data-api.com/api/cannabis/random_cannabis?size="+input;
+      document.getElementById("link").innerHTML = "https://random-data-api.com/api/cannabis/random_cannabis?size="+input;
+      const res = await axios.get(document.getElementById("link"))
       const data = res.data;
       const slice = data.slice(offset - 1 , offset - 1 + postsPerPage)
-      const postData = getData(slice)
-      setPosts(postData)
+      // const postData = getData(slice)
+      setPosts(slice)
       setPageCount(Math.ceil(data.length / postsPerPage))
+     
     }
     
-}
+    const getPosts = async () => {
+      const res = await axios.get(`https://random-data-api.com/api/cannabis/random_cannabis?size=100`)
+      const data = res.data;
+      const slice = data.slice(offset - 1 , offset - 1 + postsPerPage)
+      // const postData = getData(slice)
+      setPosts(slice)
+      setPageCount(Math.ceil(data.length / postsPerPage))
+    }
+
+    const handlePageClick = (event) => {
+      const selectedPage = event.selected;
+      setOffset(selectedPage + 1)
+    };
+    
+    useEffect(() => {
+      getPosts()
+    }, [offset])
+
+    return (
+    <div id="about">
+    <div name='random-cannabis'>
+    <div>Output: <span id="#id">1234</span></div>
+    <button onClick={()=>getPosts()}>get random 100 cannabis</button> 
+    
+
+    <a href="https://random-data-api.com/api/cannabis/random_cannabis?size=" id="link">https://random-data-api.com/api/cannabis/random_cannabis?size=</a><br></br>
+    <input type="text" id="userInput"></input><br></br>
+    <button onClick={()=>getSpec()}>get specific number cannabis</button>
+    
+    
+    
+    <ul>
+      {posts.map((post)=>{
+        return (
+          <li>
+            <div className="id" key={post.id}>
+                  <p>Item ID: {post.id}</p>
+                  <p>Item UID {post.uid}</p>
+                </div>)
+          </li>
+        )
+      })}
+
+    </ul>
+ 
+     < ReactPaginate
+       previousLabel={"<previous>" }
+       nextLabel={ "<next>" }
+       breakLabel={ "..." }
+       pageCount={ pageCount }
+       onPageChange={ handlePageClick }
+       containerClassName={ "pagination" }
+       renderOnZeroPageCount={null} / >
+
   
+    </div>
+    </div>
+    );
+}
+export default Team;
    
-  //const fireGetCannabis = function(){
+    //const fireGetCannabis = function(){
     //axios.get('https://random-data-api.com/api/cannabis/random_cannabis?size=10')
     //.then((response) => {
     //  console.log(response.data.id);
@@ -48,17 +100,7 @@ const Team = () => {
 
   //<input type='text'/> //size=100
   //{use react template loop to display {cannabisList} ul-> li}
- // return (
-   // <div id="about">
-   // <div name='random-cannabis'>
-   // <ul> https://random-data-api.com/api/cannabis/random_cannabis?size=10</ul>
-   // <div>Output: <span id="#id">1234</span></div>
-   // <button onClick={()=>fireGetCannabis()}>get random cannabis</button>
-
-   // <button>get specific number cannabis</button>
-   // </div>
-   // </div>
-  //)
+ 
     
 
-export default Team;
+
